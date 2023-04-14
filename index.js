1444b4320bd9148b6e79dddd6ff64195b6932584
7zMonkey <7zmonkey@aliyun.com>
2023-04-15 00:13:34 +0800

    优化

--- index.js
+++ index.js
@@ -1,1 +1,2 @@
-hexo.extend.filter.register('after_post_render', require('./lib/index'), 9);
+hexo.extend.filter.register('after_post_render', require('./lib/index'));
+console.log(require('./lib/index'))
